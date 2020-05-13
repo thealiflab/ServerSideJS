952b3e64f7b7c766050c6dd762e58a06e6a185f6
Alif <mail@ahmedalif.com>
2020-05-14 00:09:33 +0600

    express added successfully

--- app.js
+++ app.js
@@ -1,29 +1,64 @@
-var http = require('http');
-const fs = require('fs');
+//Using express
 
-var server = http.createServer(function(req, res) {
-    
-    var form = __dirname + "/form.html";
-    
-    fs.access(form,fs.constants.F_OK, err => {
-        //check file
-        console.log(`${form} ${err ? "HTML file does not exist" : "File Exist"}`);
-    });
-    
-    fs.readFile(form, function(err,content){
-        if(err){
-            res.writeHead(404,{"Content-type": "text/html"});
-            res.end("<h1>No such HTML file</h1>");
-        }
-        else{
-            res.writeHead(200,{"Content-type":"text/html"});
-            res.end(content);
-        }
-    });
-    
-    
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+app.use(express.static("public"));  //To use local assets
+app.use(bodyParser.urlencoded({extended: true}));
+
+////For server only
+// app.get("/nodeapp",function(req,res){
+//     res.sendFile(__dirname + "/form.html");
+// });
+
+////For local only
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/form.html");
 });
-server.listen(function(){
-    console.log(__dirname);
+
+////For server only
+// app.listen(function(){
+//     console.log("Server started....");
+// });
+
+//For local only
+app.listen(3000,function(){
+    console.log("Server started....");
+});
+
+
+////For server only
+// app.post("/nodeapp",function(req,res){
+//     var firstName = req.body.firstName;
+//     var lastName = req.body.lastName;
+//     var username = req.body.username;
+//     var email = req.body.email;
+//     var phone = req.body.phone;
+//     var gender = req.body.gender;
+
+//     console.log(firstName,lastName,username,email,phone,gender);
+
+//     //connection.end();
+//     res.sendFile(__dirname + "/success.html");
+
+// });
+
+//For local only
+app.post("/",function(req,res){
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var username = req.body.username;
+    var email = req.body.email;
+    var phone = req.body.phone;
+    var gender = req.body.gender;
+
+    console.log(firstName,lastName,username,email,phone,gender);
+
+    //connection.end();
+    res.sendFile(__dirname + "/success.html");
+
 });
 
