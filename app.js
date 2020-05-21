3c11325d639ae81fa1af5b3e67d590fbebcd9bc7
Alif <mail@ahmedalif.com>
2020-05-21 08:15:06 +0600

    data added into cpanel database successfully

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-//Using express
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
+var mysql = require("mysql");
 
 const app = express();
 
@@ -9,45 +9,24 @@ const app = express();
 app.use(express.static("public"));  //To use local assets
 app.use(bodyParser.urlencoded({extended: true}));
 
-////For server only
-// app.get("/nodeapp",function(req,res){
-//     res.sendFile(__dirname + "/form.html");
-// });
-
-////For local only
-app.get("/",function(req,res){
+app.get("/nodeapp",function(req,res){
     res.sendFile(__dirname + "/form.html");
 });
 
-////For server only
-// app.listen(function(){
-//     console.log("Server started....");
-// });
-
-//For local only
-app.listen(3000,function(){
+app.listen(function(){
     console.log("Server started....");
 });
 
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'busihfkl_formmysqlwithnodejs'
+});
 
-////For server only
-// app.post("/nodeapp",function(req,res){
-//     var firstName = req.body.firstName;
-//     var lastName = req.body.lastName;
-//     var username = req.body.username;
-//     var email = req.body.email;
-//     var phone = req.body.phone;
-//     var gender = req.body.gender;
 
-//     console.log(firstName,lastName,username,email,phone,gender);
-
-//     //connection.end();
-//     res.sendFile(__dirname + "/success.html");
-
-// });
-
-//For local only
-app.post("/",function(req,res){
+app.post("/nodeapp",function(req,res){
     var firstName = req.body.firstName;
     var lastName = req.body.lastName;
     var username = req.body.username;
@@ -56,9 +35,24 @@ app.post("/",function(req,res){
     var gender = req.body.gender;
 
     console.log(firstName,lastName,username,email,phone,gender);
+    
+    //res.send(req.body);
+    
+    var sql = "insert into usersinfo values('"+firstName+"', '"+lastName+"', '"+username+"','"+email+"', '"+phone+"', '"+gender+"')";
+    
+    
+    connection.query(sql, function (err) {
+        if (err){
+            res.sendFile(__dirname + "/fail.html");
+            console.log(err);
+        }
+        else{
+            res.sendFile(__dirname + "/success.html");
+        }
+        
+      });
 
     //connection.end();
-    res.sendFile(__dirname + "/success.html");
+    
 
 });
-
